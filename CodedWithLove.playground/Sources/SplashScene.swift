1d58ec9d3b262f215cd14b83c97752e97d12710c
Albert Sanchez <albert.san.iza@gmail.com>
2017-03-18 14:58:08 -0700

    Changed some text

--- CodedWithLove.playground/Sources/SplashScene.swift
+++ CodedWithLove.playground/Sources/SplashScene.swift
@@ -4,20 +4,25 @@ public class SplashScene: SKScene {
     override public init(size: CGSize) {
         super.init(size: size)
         backgroundColor = .black
-        let txtTitle = SKLabelNode(fontNamed: "Baskerville-Bold")
-        txtTitle.text = "CODED WITH \u{2764}"
+        let txtLove = SKLabelNode()
+        txtLove.text = "👨🏽‍💻❤️"
+        txtLove.position = CGPoint(x: frame.midX, y: frame.midY  + 42)
+        txtLove.fontSize = 50
+        addChild(txtLove)
+        let txtTitle = SKLabelNode(fontNamed: "Chalkduster")
+        txtTitle.text = "CODED WITH LOVE"
         txtTitle.position = CGPoint(x: frame.midX, y: frame.midY)
-        txtTitle.fontSize = 50
+        txtTitle.fontSize = 40
         addChild(txtTitle)
-        let txtSubTitle = SKLabelNode(fontNamed: "Apple Color Emoji")
+        let txtSubTitle = SKLabelNode(fontNamed: "Chalkduster")
         txtSubTitle.text = "By: Albert Sanchez"
-        txtSubTitle.position = CGPoint(x: frame.midX - 85 , y: frame.midY - 25)
+        txtSubTitle.position = CGPoint(x: frame.midX, y: frame.midY - 25)
         txtSubTitle.fontSize = 20
         addChild(txtSubTitle)
-        let txtStart = SKLabelNode(fontNamed: "Apple Color Emoji")
-        txtStart.text = "Start"
-        txtStart.position = CGPoint(x: frame.midX, y: 40)
-        txtStart.fontSize = 18
+        let txtStart = SKLabelNode(fontNamed: "Arial")
+        txtStart.text = "Press any key to Start"
+        txtStart.position = CGPoint(x: frame.midX, y: 20)
+        txtStart.fontSize = 16
         addChild(txtStart)
     }
     required public init?(coder aDecoder: NSCoder) {
