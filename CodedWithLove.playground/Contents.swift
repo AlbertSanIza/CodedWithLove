67bea230116a3b6b87d03bd7a48bb0410b47c771
Albert Sanchez <albert.san.iza@gmail.com>
2017-03-18 14:39:55 -0700

    Went back to splashscreen

--- CodedWithLove.playground/Contents.swift
+++ CodedWithLove.playground/Contents.swift
@@ -5,7 +5,7 @@ let mainFrame = CGRect(x: 0, y: 0, width: 500, height: 300)
 let mainView = SKView(frame: mainFrame)
 mainView.showsFPS = true
 
-let splashScene = MainMenuScene(size: mainFrame.size)
+let splashScene = SplashScene(size: mainFrame.size)
 mainView.presentScene(splashScene)
 
 PlaygroundPage.current.liveView = mainView
