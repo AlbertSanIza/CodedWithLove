a0fbdfe10501fae3d83420bd1366e880d0284e74
Albert Sanchez <albert.san.iza@gmail.com>
2017-03-18 20:12:24 -0700

    Increased size

--- CodedWithLove.playground/Contents.swift
+++ CodedWithLove.playground/Contents.swift
@@ -1,7 +1,7 @@
 import SpriteKit
 import PlaygroundSupport
 
-let mainFrame = CGRect(x: 0, y: 0, width: 500, height: 300)
+let mainFrame = CGRect(x: 0, y: 0, width: 600, height: 350)
 let mainView = SKView(frame: mainFrame)
 mainView.showsFPS = true
 
