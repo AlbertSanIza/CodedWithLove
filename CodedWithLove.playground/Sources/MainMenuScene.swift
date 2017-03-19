8580ecb85c2a5648b8b8cdb5b46d0194484f039e
Albert Sanchez <albert.san.iza@gmail.com>
2017-03-18 20:32:39 -0700

    Updated MainMenuScene

---
+++ CodedWithLove.playground/Sources/MainMenuScene.swift
@@ -0,0 +1,88 @@
+import SpriteKit
+import GameplayKit
+public class MainMenuScene: SKScene {
+    var entities = [GKEntity]()
+    var graphs = [String : GKGraph]()
+    private var lastUpdateTime : TimeInterval = 0
+    private var label : SKLabelNode?
+    private var spinnyNode : SKShapeNode?
+    override public init(size: CGSize) {
+        super.init(size: size)
+        let txtHello = SKLabelNode(fontNamed: "Helvetica Neue UltraLight")
+        addChild(txtHello)
+        txtHello.name = "helloLabel"
+        txtHello.text = "Hello, World! 🇲🇽"
+        txtHello.position = CGPoint(x: frame.midX, y: frame.midY - 25)
+        txtHello.fontSize = 50
+        txtHello.zPosition = 1
+    }
+    override public func sceneDidLoad() {
+        self.lastUpdateTime = 0
+        self.label = self.childNode(withName: "//helloLabel") as? SKLabelNode
+        if let label = self.label {
+            label.alpha = 0.0
+            label.run(SKAction.fadeIn(withDuration: 2.0))
+        }
+        let w = (self.size.width + self.size.height) * 0.05
+        self.spinnyNode = SKShapeNode.init(rectOf: CGSize.init(width: w, height: w), cornerRadius: w * 0.3)
+        
+        if let spinnyNode = self.spinnyNode {
+            spinnyNode.lineWidth = 2.5
+            spinnyNode.run(SKAction.repeatForever(SKAction.rotate(byAngle: CGFloat(M_PI), duration: 1)))
+            spinnyNode.run(SKAction.sequence([SKAction.wait(forDuration: 0.5), SKAction.fadeOut(withDuration: 0.5), SKAction.removeFromParent()]))
+        }
+    }
+    func touchDown(atPoint pos : CGPoint) {
+        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
+            n.position = pos
+            n.strokeColor = SKColor.green
+            self.addChild(n)
+        }
+    }
+    func touchMoved(toPoint pos : CGPoint) {
+        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
+            n.position = pos
+            n.strokeColor = SKColor.blue
+            self.addChild(n)
+        }
+    }
+    func touchUp(atPoint pos : CGPoint) {
+        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
+            n.position = pos
+            n.strokeColor = SKColor.red
+            self.addChild(n)
+        }
+    }
+    override public func mouseDown(with event: NSEvent) {
+        self.touchDown(atPoint: event.location(in: self))
+    }
+    override public func mouseDragged(with event: NSEvent) {
+        self.touchMoved(toPoint: event.location(in: self))
+    }
+    override public func mouseUp(with event: NSEvent) {
+        self.touchUp(atPoint: event.location(in: self))
+    }
+    override public func keyDown(with event: NSEvent) {
+        switch event.keyCode {
+        case 0x31:
+            if let label = self.label {
+                label.run(SKAction.init(named: "Pulse")!, withKey: "fadeInOut")
+            }
+        default:
+            print("keyDown: \(event.characters!) keyCode: \(event.keyCode)")
+        }
+    }
+    override public func update(_ currentTime: TimeInterval) {
+        if (self.lastUpdateTime == 0) {
+            self.lastUpdateTime = currentTime
+        }
+        let dt = currentTime - self.lastUpdateTime
+        for entity in self.entities {
+            entity.update(deltaTime: dt)
+        }
+        self.lastUpdateTime = currentTime
+    }
+    required public init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
