6a248ce2b5cfe0774d0011566a12e574aab54871
Albert Sanchez <albert.san.iza@gmail.com>
2017-03-18 20:48:58 -0700

    Changed text

--- CodedWithLove.playground/Sources/SplashScene.swift
+++ CodedWithLove.playground/Sources/SplashScene.swift
@@ -18,7 +18,7 @@ public class SplashScene: SKScene {
         txtSubTitle.fontSize = 20
         addChild(txtSubTitle)
         let txtStart = SKLabelNode()
-        txtStart.text = "Press any key to Start"
+        txtStart.text = "Press spacebar to start..."
         txtStart.position = CGPoint(x: frame.midX, y: 20)
         txtStart.fontSize = 20
         addChild(txtStart)
