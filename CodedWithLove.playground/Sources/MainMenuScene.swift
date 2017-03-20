595257cb39e780639146974397c4e9eccb4fb503
Albert Sanchez <albert.san.iza@gmail.com>
2017-03-20 13:36:52 -0700

    S to start

--- CodedWithLove.playground/Sources/MainMenuScene.swift
+++ CodedWithLove.playground/Sources/MainMenuScene.swift
@@ -42,8 +42,9 @@ public class MainMenuScene: SKScene {
         }
     }
     override public func keyUp(with event: NSEvent) {
+        print(event.keyCode)
         switch event.keyCode {
-        case 1:
+        case 1, 36:
             run(SKAction.playSoundFileNamed("sounds/pick.wav" , waitForCompletion: false))
             goToScene(withName: "StartScene")
         case 34:
