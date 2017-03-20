015aeee6689a761fb5a0d078f832f7832f2972eb
Albert Sanchez <albert.san.iza@gmail.com>
2017-03-19 21:46:01 -0700

    Fixed aboutMe Scene

--- CodedWithLove.playground/Sources/AboutMeScene.swift
+++ CodedWithLove.playground/Sources/AboutMeScene.swift
@@ -1,19 +1,20 @@
 import SpriteKit
-
 public class AboutMeScene: SKScene {
-    required public init?(coder aDecoder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
     override public init(size: CGSize) {
         super.init(size: size)
         let txtTitle = SKLabelNode()
-        txtTitle.text = "About Me  "
+        txtTitle.text = "About Me"
         txtTitle.position = CGPoint(x: frame.midX, y: frame.height - 40)
         txtTitle.fontSize = 40
         addChild(txtTitle)
     }
     override public func keyUp(with event: NSEvent) {
-        let mainMenuScene = MainMenuScene(size: (scene?.size)!)
-        self.view?.presentScene(mainMenuScene)
+        goToScene()
+    }
+    func goToScene() {
+        self.view?.presentScene(MainMenuScene(size: (scene?.size)!), transition: SKTransition.fade(withDuration: 1.5))
+    }
+    required public init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
     }
 }
