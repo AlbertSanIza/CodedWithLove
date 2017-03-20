b47a81c9a4798d6223ef154412f8277a2b403ce4
Albert Sanchez <albert.san.iza@gmail.com>
2017-03-20 11:12:48 -0700

    Added some messages to about

--- CodedWithLove.playground/Sources/AboutScene.swift
+++ CodedWithLove.playground/Sources/AboutScene.swift
@@ -14,6 +14,28 @@ public class AboutScene: SKScene {
         txtBack.position = CGPoint(x: 50, y: frame.height - 55)
         txtBack.fontSize = 20
         addChild(txtBack)
+        let nodeText = SKNode()
+        nodeText.name = "nodeText"
+        nodeText.position = CGPoint(x: frame.midX, y: frame.midY)
+        addChild(nodeText)
+        let txtMessage01 = SKLabelNode()
+        txtMessage01.name = "txtMessage01"
+        txtMessage01.text = " WWDC 2017 Scholarship Entry"
+        txtMessage01.position = CGPoint(x: 0, y: 50)
+        txtMessage01.fontSize = 20
+        nodeText.addChild(txtMessage01)
+        let txtMessage02 = SKLabelNode()
+        txtMessage02.name = "txtMessage02"
+        txtMessage02.text = "CODED WITH LOVE"
+        txtMessage02.position = CGPoint(x: 0, y: 25)
+        txtMessage02.fontSize = 20
+        nodeText.addChild(txtMessage02)
+        let txtMessage03 = SKLabelNode()
+        txtMessage03.name = "txtMessage03"
+        txtMessage03.text = "by: Albert Sanchez"
+        txtMessage03.position = CGPoint(x: 0, y: 0)
+        txtMessage03.fontSize = 20
+        nodeText.addChild(txtMessage02)
     }
     override public func keyUp(with event: NSEvent) {
         switch event.keyCode {
