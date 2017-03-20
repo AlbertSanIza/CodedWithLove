a2d74d372e7da9f9fed29d16e71369e32f440e8d
Albert Sanchez <albert.san.iza@gmail.com>
2017-03-20 10:50:25 -0700

    Re arranged add Childs

--- CodedWithLove.playground/Sources/MainMenuScene.swift
+++ CodedWithLove.playground/Sources/MainMenuScene.swift
@@ -6,36 +6,36 @@ public class MainMenuScene: SKScene {
         txtTitle.text = "Main Menu"
         txtTitle.position = CGPoint(x: frame.midX, y: frame.height - 60)
         txtTitle.fontSize = 40
+        addChild(txtTitle)
         let nodeMenu = SKNode()
         nodeMenu.name = "nodeMenu"
         nodeMenu.position = CGPoint(x: frame.midX, y: frame.midY - 30)
         nodeMenu.alpha = 0.0
+        addChild(nodeMenu)
         let txtStart = SKLabelNode()
         txtStart.name = "txtStart"
         txtStart.text = "START"
         txtStart.position = CGPoint(x: 0, y: 0)
         txtStart.fontSize = 80
+        nodeMenu.addChild(txtStart)
         let txtInstructions = SKLabelNode()
         txtInstructions.name = "txtInstructions"
         txtInstructions.text = "Instructions"
         txtInstructions.position = CGPoint(x: 0, y: -40)
         txtInstructions.fontSize = 30
+        nodeMenu.addChild(txtInstructions)
         let txtAboutMe = SKLabelNode()
         txtAboutMe.name = "txtAboutMe"
         txtAboutMe.text = "About Me"
         txtAboutMe.position = CGPoint(x: 0, y: -80)
         txtAboutMe.fontSize = 30
+        nodeMenu.addChild(txtAboutMe)
         let txtBack = SKLabelNode()
         txtBack.name = "txtBack"
         txtBack.text = "Back"
         txtBack.position = CGPoint(x: 0, y: -120)
         txtBack.fontSize = 20
-        nodeMenu.addChild(txtStart)
-        nodeMenu.addChild(txtInstructions)
-        nodeMenu.addChild(txtAboutMe)
         nodeMenu.addChild(txtBack)
-        addChild(txtTitle)
-        addChild(nodeMenu)
         let sequence = SKAction.repeatForever(SKAction.sequence([SKAction.fadeOut(withDuration: 0.5), SKAction.fadeIn(withDuration: 2)]))
         nodeMenu.run(SKAction.group([SKAction.moveTo(y: frame.midY, duration: 1.0), SKAction.sequence([SKAction.fadeIn(withDuration: 1.5)])])) {
             txtStart.run(sequence)
