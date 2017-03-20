c85dfb988d765986be3666936881cce911800b43
Albert Sanchez <albert.san.iza@gmail.com>
2017-03-20 09:42:34 -0700

    Added back button to instructions scene

--- CodedWithLove.playground/Sources/InstructionsScene.swift
+++ CodedWithLove.playground/Sources/InstructionsScene.swift
@@ -7,12 +7,30 @@ public class InstructionsScene: SKScene {
         txtTitle.position = CGPoint(x: frame.midX, y: frame.height - 60)
         txtTitle.fontSize = 40
         addChild(txtTitle)
+        let txtBack = SKLabelNode()
+        txtBack.name = "txtBack"
+        txtBack.text = "Back"
+        txtBack.position = CGPoint(x: 50, y: frame.height - 55)
+        txtBack.fontSize = 20
+        addChild(txtBack)
     }
     override public func keyUp(with event: NSEvent) {
         switch event.keyCode {
         case 53:
             goToScene()
         default:break
+        }
+    }
+    override public func mouseDown(with event: NSEvent) {
+        let positionInScene = CGPoint(x: event.locationInWindow.x, y: event.locationInWindow.y)
+        if let touchedNode = self.nodes(at: positionInScene).first {
+            switch touchedNode.name {
+            case "txtBack"?:
+                touchedNode.run(SKAction.fadeOut(withDuration: 0.5)) {
+                    self.goToScene()
+                }
+            default: break
+            }
         }
     }
     func goToScene() {
