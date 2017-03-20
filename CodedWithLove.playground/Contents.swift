e32722f0b931f84ecae53bcf6207f7fffa1de872
Albert Sanchez <albert.san.iza@gmail.com>
2017-03-20 15:30:10 -0700

    Show node count

--- CodedWithLove.playground/Contents.swift
+++ CodedWithLove.playground/Contents.swift
@@ -4,6 +4,7 @@ import PlaygroundSupport
 let mainFrame = CGRect(x: 0, y: 0, width: 700, height: 400)
 let mainView = SKView(frame: mainFrame)
 mainView.showsFPS = true
+mainView.showsNodeCount = true
 
 let splashScene = SplashScene(size: mainFrame.size)
 mainView.presentScene(splashScene)
