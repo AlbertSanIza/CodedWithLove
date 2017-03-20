5979538195c3ff241baed320c2690857dcfe51a9
Albert Sanchez <albert.san.iza@gmail.com>
2017-03-20 10:54:05 -0700

    Added back button

--- CodedWithLove.playground/Sources/StartScene.swift
+++ CodedWithLove.playground/Sources/StartScene.swift
@@ -7,12 +7,32 @@ public class StartScene: SKScene {
         txtTitle.position = CGPoint(x: frame.midX, y: frame.height - 60)
         txtTitle.fontSize = 40
         addChild(txtTitle)
+        let txtBack = SKLabelNode()
+        txtBack.name = "txtBack"
+        txtBack.text = "Back"
+        txtBack.position = CGPoint(x: 50, y: frame.height - 55)
+        txtBack.fontSize = 20
+        addChild(txtBack)
     }
     override public func keyUp(with event: NSEvent) {
         switch event.keyCode {
         case 53:
+            run(SKAction.playSoundFileNamed("sounds/pick.wav" , waitForCompletion: false))
             goToScene()
         default:break
+        }
+    }
+    override public func mouseDown(with event: NSEvent) {
+        let positionInScene = CGPoint(x: event.locationInWindow.x, y: event.locationInWindow.y)
+        if let touchedNode = nodes(at: positionInScene).first {
+            switch touchedNode.name {
+            case "txtBack"?:
+                run(SKAction.playSoundFileNamed("sounds/pick.wav" , waitForCompletion: false))
+                touchedNode.run(SKAction.fadeOut(withDuration: 0.25)) {
+                    self.goToScene()
+                }
+            default: break
+            }
         }
     }
     func goToScene() {
