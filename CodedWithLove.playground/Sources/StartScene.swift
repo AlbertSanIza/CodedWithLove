858b0baf5347049104843e20cdbe231e7033959d
Albert Sanchez <albert.san.iza@gmail.com>
2017-03-19 22:05:55 -0700

    Created StartScene file

---
+++ CodedWithLove.playground/Sources/StartScene.swift
@@ -0,0 +1,20 @@
+import SpriteKit
+public class StartScene: SKScene {
+    override public init(size: CGSize) {
+        super.init(size: size)
+        let txtTitle = SKLabelNode()
+        txtTitle.text = "Start"
+        txtTitle.position = CGPoint(x: frame.midX, y: frame.height - 60)
+        txtTitle.fontSize = 40
+        addChild(txtTitle)
+    }
+    override public func keyUp(with event: NSEvent) {
+        goToScene()
+    }
+    func goToScene() {
+        self.view?.presentScene(MainMenuScene(size: (scene?.size)!), transition: SKTransition.fade(withDuration: 1.5))
+    }
+    required public init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
