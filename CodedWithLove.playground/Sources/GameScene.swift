d89b43c8fc5ac3b69c2a6e51f641e45e68096b5f
Albert Sanchez <albert.san.iza@gmail.com>
2017-03-21 22:48:17 -0700

    auto indent

--- CodedWithLove.playground/Sources/GameScene.swift
+++ CodedWithLove.playground/Sources/GameScene.swift
@@ -1,14 +1,14 @@
 import SpriteKit
 public class GameSceneFile: SKScene {
-  override public func sceneDidLoad() {
-    super.sceneDidLoad()
-  }
-  override public func keyUp(with event: NSEvent) {
-    run(SKAction.playSoundFileNamed("sounds/pick.wav" , waitForCompletion: false))
-    goToScene()
-  }
-  func goToScene() {
-  }
+    override public func sceneDidLoad() {
+        super.sceneDidLoad()
+    }
+    override public func keyUp(with event: NSEvent) {
+        run(SKAction.playSoundFileNamed("sounds/pick.wav" , waitForCompletion: false))
+        goToScene()
+    }
+    func goToScene() {
+    }
 }
 public class GameScene: SKScene {
     override public func sceneDidLoad() {
