3e0bafce7f2ca396229162055f4a6801ff5a6450
Albert Sanchez <albert.san.iza@gmail.com>
2017-03-21 22:45:28 -0700

    removes unused code

--- CodedWithLove.playground/Sources/InstructionsScene.swift
+++ CodedWithLove.playground/Sources/InstructionsScene.swift
@@ -1,41 +1,19 @@
 import SpriteKit
 public class InstructionsSceneFile: SKScene {
-  override public func sceneDidLoad() {
-    super.sceneDidLoad()
-  }
-  override public func keyUp(with event: NSEvent) {
-    run(SKAction.playSoundFileNamed("sounds/pick.wav" , waitForCompletion: false))
-    goToScene()
-  }
-  func goToScene() {
-  }
-}
-public class InstructionsScene: SKScene {
-    override public init(size: CGSize) {
-        super.init(size: size)
-        let txtTitle = SKLabelNode()
-        txtTitle.text = "Instructions"
-        txtTitle.position = CGPoint(x: frame.midX, y: frame.height - 60)
-        txtTitle.fontSize = 40
-        addChild(txtTitle)
-        let txtBack = SKLabelNode()
-        txtBack.name = "txtBack"
-        txtBack.text = "Back"
-        txtBack.position = CGPoint(x: 50, y: frame.height - 55)
-        txtBack.fontSize = 20
-        addChild(txtBack)
+    override public func sceneDidLoad() {
+        super.sceneDidLoad()
     }
     override public func keyUp(with event: NSEvent) {
         switch event.keyCode {
         case 53, 11:
             run(SKAction.playSoundFileNamed("sounds/pick.wav" , waitForCompletion: false))
             goToScene()
-        default:break
+        default: break
         }
     }
     override public func mouseDown(with event: NSEvent) {
-        let positionInScene = CGPoint(x: event.locationInWindow.x, y: event.locationInWindow.y)
-        if let touchedNode = nodes(at: positionInScene).first {
+        let mousePoint = convertPoint(fromView: CGPoint(x: event.locationInWindow.x, y: event.locationInWindow.y))
+        if let touchedNode = nodes(at: mousePoint).first {
             switch touchedNode.name {
             case "txtBack"?:
                 run(SKAction.playSoundFileNamed("sounds/pick.wav" , waitForCompletion: false))
@@ -47,9 +25,8 @@ public class InstructionsScene: SKScene {
         }
     }
     func goToScene() {
-        self.view?.presentScene(MainMenuSceneFile(size: (scene?.size)!), transition: SKTransition.crossFade(withDuration: 1.0))
-    }
-    required public init?(coder aDecoder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
+        let mainMenuScene: SKScene = MainMenuSceneFile(fileNamed: "scenes/MainMenuScene.sks")!
+        mainMenuScene.scaleMode = .aspectFit
+        view?.presentScene(mainMenuScene, transition: SKTransition.fade(withDuration: 2.0))
     }
 }
