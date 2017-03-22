dd56f0db8d46585cd88b2037ee8cc112987a081e
Albert Sanchez <albert.san.iza@gmail.com>
2017-03-21 22:37:40 -0700

    go to scene on click

--- CodedWithLove.playground/Sources/StartScene.swift
+++ CodedWithLove.playground/Sources/StartScene.swift
@@ -11,7 +11,23 @@ public class StartSceneFile: SKScene {
     default: break
     }
   }
-  func goToScene() {
+    override public func mouseDown(with event: NSEvent) {
+        let mousePoint = convertPoint(fromView: CGPoint(x: event.locationInWindow.x, y: event.locationInWindow.y))
+        if let touchedNode = nodes(at: mousePoint).first {
+            switch touchedNode.name {
+            case "txtBack"?:
+                run(SKAction.playSoundFileNamed("sounds/pick.wav" , waitForCompletion: false))
+                touchedNode.run(SKAction.fadeOut(withDuration: 0.5)) {
+                    self.goToScene()
+                }
+            default: break
+            }
+        }
+    }
+    func goToScene() {
+    let mainMenuScene: SKScene = MainMenuSceneFile(fileNamed: "scenes/MainMenuScene.sks")!
+    mainMenuScene.scaleMode = .aspectFit
+    view?.presentScene(mainMenuScene, transition: SKTransition.fade(withDuration: 2.0))
   }
 }
 public class StartScene: SKScene {
