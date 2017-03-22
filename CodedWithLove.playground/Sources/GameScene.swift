f98cbc98e999be987cbbc5a86b3ab0895f677e5f
Albert Sanchez <albert.san.iza@gmail.com>
2017-03-21 17:31:59 -0700

    Fixed typo

--- CodedWithLove.playground/Sources/GameScene.swift
+++ CodedWithLove.playground/Sources/GameScene.swift
@@ -3,7 +3,7 @@ public class GameSceneFile: SKScene {
   override public func sceneDidLoad() {
     super.sceneDidLoad()
   }
-  override public func keyUp(with event: NSEven) {
+  override public func keyUp(with event: NSEvent) {
     run(SKAction.playSoundFileNamed("sounds/pick.wav" , waitForCompletion: false))
     goToScene()
   }
