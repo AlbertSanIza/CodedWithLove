42d597ea382c66afe1d44acb5c8ededb8a915130
Albert Sanchez <albert.san.iza@gmail.com>
2017-03-23 23:21:00 -0700

    added some random options to scene creation

--- CodedWithLove.playground/Sources/GameScene.swift
+++ CodedWithLove.playground/Sources/GameScene.swift
@@ -3,11 +3,28 @@ public class GameSceneFile: SKScene {
     override public func sceneDidLoad() {
         super.sceneDidLoad()
         if let sceneBackground: SKSpriteNode = childNode(withName: "sksBackground") as! SKSpriteNode? {
-            sceneBackground.texture = SKTexture(imageNamed: "sprites/spaceBackground/backgrounds2.png")
+            var randomNumber = arc4random_uniform(4 - 1) + 1
+            sceneBackground.texture = SKTexture(imageNamed: "sprites/spaceBackground/backgrounds" + String(randomNumber) + ".png")
             if let sceneClouds: SKSpriteNode = childNode(withName: "sksClouds") as! SKSpriteNode? {
-                sceneClouds.texture = SKTexture(imageNamed: "sprites/spaceBackground/orangeClouds2.png")
+                randomNumber = arc4random_uniform(4 - 1) + 1
+                let cloudsColor: String
+                switch randomNumber {
+                case 1:
+                    cloudsColor = "green"
+                case 2:
+                    cloudsColor = "orange"
+                case 3:
+                    cloudsColor = "purple"
+                case 4:
+                    cloudsColor = "red"
+                default:
+                    cloudsColor = "red"
+                }
+                randomNumber = arc4random_uniform(3 - 1) + 1
+                sceneClouds.texture = SKTexture(imageNamed: "sprites/spaceBackground/" + cloudsColor + "Clouds" + String(randomNumber) + ".png")
                 if let sceneStars: SKSpriteNode = childNode(withName: "sksStars") as! SKSpriteNode? {
-                    sceneStars.texture = SKTexture(imageNamed: "sprites/spaceBackground/starsLayer2.png")
+                    randomNumber = arc4random_uniform(3 - 1) + 1
+                    sceneStars.texture = SKTexture(imageNamed: "sprites/spaceBackground/starsLayer" + String(randomNumber) + ".png")
                 }
             }
         }
