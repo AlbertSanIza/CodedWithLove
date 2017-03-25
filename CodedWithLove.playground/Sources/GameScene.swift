cf9d334f723180df5186fe8ea581293625a1d961
Albert Sanchez <albert.san.iza@gmail.com>
2017-03-25 15:22:24 -0700

    Fixed typo

--- CodedWithLove.playground/Sources/GameScene.swift
+++ CodedWithLove.playground/Sources/GameScene.swift
@@ -97,7 +97,7 @@ public class GameSceneFile: SKScene {
         toGoScene.scaleMode = .aspectFit
         view?.presentScene(toGoScene, transition: SKTransition.crossFade(withDuration: 2.0))
     }
-    func radiansToDegress(radians: CGFloat) -> CGFloat {
+    func radiansToDegrees(radians: CGFloat) -> CGFloat {
         return radians * 180 / CGFloat(M_PI)
     }
     func degreesToRadians(degrees: CGFloat) -> CGFloat {
