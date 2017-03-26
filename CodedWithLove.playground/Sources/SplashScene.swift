f63b9822431234dd80c4eaaa8085049c580b1006
Albert Sanchez <albert.san.iza@gmail.com>
2017-03-25 22:21:55 -0700

    load starsLayer png

--- CodedWithLove.playground/Sources/SplashScene.swift
+++ CodedWithLove.playground/Sources/SplashScene.swift
@@ -2,6 +2,9 @@ import SpriteKit
 public class SplashSceneFile: SKScene {
     override public func sceneDidLoad() {
         super.sceneDidLoad()
+        if let sceneStars: SKSpriteNode = childNode(withName: "sksStars") as! SKSpriteNode? {
+            sceneStars.texture = SKTexture(imageNamed: "sprites/background/starsLayer2.png")
+        }
         run(SKAction.sequence([SKAction.wait(forDuration: 1.0), SKAction.playSoundFileNamed("sounds/splash.wav" , waitForCompletion: false)]))
     }
     override public func keyUp(with event: NSEvent) {
