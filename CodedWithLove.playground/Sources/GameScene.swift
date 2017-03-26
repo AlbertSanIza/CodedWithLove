90b30cbbe37e4698e07acabeecd56b22fc1d9777
Albert Sanchez <albert.san.iza@gmail.com>
2017-03-26 12:28:34 -0700

    no longer needed on skpritenode

--- CodedWithLove.playground/Sources/GameScene.swift
+++ CodedWithLove.playground/Sources/GameScene.swift
@@ -52,15 +52,9 @@ public class GameSceneFile: SKScene {
         }
         if aKey {
             thePlayer.zRotation += degreesToRadians(degrees: 4)
-            if radiansToDegrees(radians: thePlayer.zRotation) > 359 {
-                thePlayer.zRotation = degreesToRadians(degrees: 0)
-            }
         }
         if dKey {
             thePlayer.zRotation -= degreesToRadians(degrees: 4)
-            if radiansToDegrees(radians: thePlayer.zRotation) < 0 {
-                thePlayer.zRotation = degreesToRadians(degrees: 359)
-            }
         }
         if thePlayer.position.y > 400 {
             thePlayer.position.y = -398
