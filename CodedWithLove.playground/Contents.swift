3ef03f50ae600ca2509e8830330e1b45b57b9d97
Albert Sanchez <albert.san.iza@gmail.com>
2017-03-26 13:39:36 -0700

    fixed tru bug

--- CodedWithLove.playground/Contents.swift
+++ CodedWithLove.playground/Contents.swift
@@ -5,7 +5,7 @@ import PlaygroundSupport
 let mainFrame = CGRect(x: 0, y: 0, width: 1024, height: 768)
 let mainView = SKView(frame: mainFrame)
 mainView.showsFPS = true
-mainView.showsNodeCount = tru
+mainView.showsNodeCount = true
 
 let splashScene: SKScene = GameSceneFile(fileNamed: "scenes/GameScene.sks")!
 splashScene.scaleMode = .aspectFit
