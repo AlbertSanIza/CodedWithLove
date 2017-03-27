2036eaa54b54585b7a83fd879d5be9effece407a
Albert Sanchez <albert.san.iza@gmail.com>
2017-03-26 23:47:42 -0700

    Changed frame size for laptop dev

--- CodedWithLove.playground/Contents.swift
+++ CodedWithLove.playground/Contents.swift
@@ -1,8 +1,8 @@
 import SpriteKit
 import PlaygroundSupport
 
-//let mainFrame = CGRect(x: 0, y: 0, width: 600, height: 450)
-let mainFrame = CGRect(x: 0, y: 0, width: 1024, height: 768)
+let mainFrame = CGRect(x: 0, y: 0, width: 600, height: 450)
+//let mainFrame = CGRect(x: 0, y: 0, width: 1024, height: 768)
 let mainView = SKView(frame: mainFrame)
 mainView.showsFPS = true
 mainView.showsNodeCount = false
