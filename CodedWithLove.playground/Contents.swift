f6828b31edf71f74dfd7c3469da310eee0358211
Albert Sanchez <albert.san.iza@gmail.com>
2017-03-27 00:06:17 -0700

    Show node count

--- CodedWithLove.playground/Contents.swift
+++ CodedWithLove.playground/Contents.swift
@@ -5,7 +5,7 @@ let mainFrame = CGRect(x: 0, y: 0, width: 600, height: 450)
 //let mainFrame = CGRect(x: 0, y: 0, width: 1024, height: 768)
 let mainView = SKView(frame: mainFrame)
 mainView.showsFPS = true
-mainView.showsNodeCount = false
+mainView.showsNodeCount = true
 mainView.showsPhysics = false
 
 let splashScene: SKScene = GameSceneFile(fileNamed: "scenes/GameScene.sks")!
