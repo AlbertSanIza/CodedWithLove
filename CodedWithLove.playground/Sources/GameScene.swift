4a6599f15958cf189bbf7231616b75d1831bbbc3
Albert Sanchez <albert.san.iza@gmail.com>
2017-03-26 19:00:18 -0700

    added a PhysicsCategory

--- CodedWithLove.playground/Sources/GameScene.swift
+++ CodedWithLove.playground/Sources/GameScene.swift
@@ -1,4 +1,10 @@
 import SpriteKit
+struct PhysicsCategory {
+    static let None: UInt32 = 0
+    static let All: UInt32 = UInt32.max
+    static let Asteroid: UInt32 = 0b1
+    static let Projectile: UInt32 = 0b10
+}
 public class GameSceneFile: SKScene {
     var thePlayer: SKSpriteNode = SKSpriteNode()
     var thePause: SKNode = SKNode()
