726ad0d2b5fd5ecee0f287ab8f28f3b6201b7825
Albert Sanchez <albert.san.iza@gmail.com>
2017-03-28 21:28:28 -0700

    changed size and physics for art development

--- CodedWithLove.playground/Contents.swift
+++ CodedWithLove.playground/Contents.swift
@@ -1,14 +1,14 @@
 import SpriteKit
 import PlaygroundSupport
 
-//let mainFrame = CGRect(x: 0, y: 0, width: 1024, height: 768)
-let mainFrame = CGRect(x: 0, y: 0, width: 750, height: 563)
+let mainFrame = CGRect(x: 0, y: 0, width: 1024, height: 768)
+//let mainFrame = CGRect(x: 0, y: 0, width: 750, height: 563)
 //let mainFrame = CGRect(x: 0, y: 0, width: 600, height: 450)
 
 let mainView = SKView(frame: mainFrame)
 mainView.showsFPS = true
 mainView.showsNodeCount = true
-mainView.showsPhysics = true
+mainView.showsPhysics = false
 
 let splashScene: SKScene = GameSceneFile(fileNamed: "scenes/GameScene.sks")!
 splashScene.scaleMode = .aspectFit
