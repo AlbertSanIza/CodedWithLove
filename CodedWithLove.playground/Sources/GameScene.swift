b02ffedd619a1c8fc3e8b37614dd669c87d13f57
Albert Sanchez <albert.san.iza@gmail.com>
2017-03-28 22:31:40 -0700

    Create sksnode with size

--- CodedWithLove.playground/Sources/GameScene.swift
+++ CodedWithLove.playground/Sources/GameScene.swift
@@ -283,7 +283,7 @@ public class GameSceneFile: SKScene, SKPhysicsContactDelegate {
         return shield
     }
     func addPowerUp(inPosition: CGPoint) {
-        let powerUp: SKSpriteNode = SKSpriteNode(imageNamed: "sprites/artRedux/powerUpShield.png")
+        let powerUp = SKSpriteNode(texture: SKTexture(imageNamed: "sprites/artRedux/powerUpShield.png"), size: CGSize(width: 60, height: 60))
         powerUp.position = inPosition
         powerUp.physicsBody = SKPhysicsBody(circleOfRadius: 135, center: CGPoint(x: 0, y: 0))
         powerUp.physicsBody?.categoryBitMask = PhysicsCategory.PowerUp
