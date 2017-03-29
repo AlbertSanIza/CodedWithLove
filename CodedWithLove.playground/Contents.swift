f45c4c028c801a57c4a65e9344ebdef636ad898b
Albert Sanchez <albert.san.iza@gmail.com>
2017-03-28 22:04:43 -0700

    Changed size for small screen development

--- CodedWithLove.playground/Contents.swift
+++ CodedWithLove.playground/Contents.swift
@@ -1,8 +1,8 @@
 import SpriteKit
 import PlaygroundSupport
 
-let mainFrame = CGRect(x: 0, y: 0, width: 1024, height: 768)
-//let mainFrame = CGRect(x: 0, y: 0, width: 750, height: 563)
+//let mainFrame = CGRect(x: 0, y: 0, width: 1024, height: 768)
+let mainFrame = CGRect(x: 0, y: 0, width: 750, height: 563)
 //let mainFrame = CGRect(x: 0, y: 0, width: 600, height: 450)
 
 let mainView = SKView(frame: mainFrame)
