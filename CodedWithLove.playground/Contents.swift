cad1ecf03746c16b0e9741b4d7324b11d93d0f40
Albert Sanchez <albert.san.iza@gmail.com>
2017-03-31 10:45:09 -0700

    Added a message for the judges

--- CodedWithLove.playground/Contents.swift
+++ CodedWithLove.playground/Contents.swift
@@ -1,6 +1,15 @@
 import SpriteKit
 import PlaygroundSupport
 
+/* 
+ Hello Hudge,
+ 
+ This playground was intendent to look properly on a 1024x768 size, but if its too big (like in my case) you just have to cahnge the widht and height values, or you can uncomment some other suggested sizes
+ 
+ sorry for the inconvinience ❤️
+ 
+ */
+
 let mainFrame = CGRect(x: 0, y: 0, width: 1024, height: 768)
 //let mainFrame = CGRect(x: 0, y: 0, width: 750, height: 563)
 //let mainFrame = CGRect(x: 0, y: 0, width: 600, height: 450)
