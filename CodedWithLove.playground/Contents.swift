0102e76ab515448700d08e3c18449aa13e71243b
Albert Sanchez <albert.san.iza@gmail.com>
2017-03-30 18:24:32 -0700

    Removed dev data

--- CodedWithLove.playground/Contents.swift
+++ CodedWithLove.playground/Contents.swift
@@ -6,8 +6,8 @@ let mainFrame = CGRect(x: 0, y: 0, width: 750, height: 563)
 //let mainFrame = CGRect(x: 0, y: 0, width: 600, height: 450)
 
 let mainView = SKView(frame: mainFrame)
-mainView.showsFPS = true
-mainView.showsNodeCount = true
+mainView.showsFPS = false
+mainView.showsNodeCount = false
 mainView.showsPhysics = false
 
 let splashScene: SKScene = SplashSceneFile(fileNamed: "scenes/SplashScene.sks")!
